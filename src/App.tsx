e01cd2b8a7531391422913b4cb4b25083f31d43d
bohdanalu <b_dana@ukr.net>
2025-06-23 13:11:59 +0300

    Add initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+     
+       <h1 className='text-3xl font-bold text-primary'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
